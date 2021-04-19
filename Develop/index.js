6a6d0b4bea9122566014ccd801614d73c0402dc3
Zack <ahmedsakaria32@gmail.com>
2021-04-18 19:58:33 -0500

    starting to create the Readme file

--- Develop/index.js
+++ Develop/index.js
@@ -4,64 +4,70 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [ 
 
-    {
+    {///Project name
 
         type: "input",
-        name: "projectName",
+        name: "title",
         message: "What Do you wanna name the project?"
     },
-    {
+    {//The author or creator of the project
 
         type: "input",
         name: "Author",
         message: "What the name of the person who created this project?"
     },
-    {
+    {// The collaborators
+
+        type: "input",
+        name: "Collaborators",
+        message: "Who else contributed to the project?"
+    },
+    {///Your contact info email
 
         type: "input",
         name: "Email",
         message: "What is your email address?",
     },
-    {
+    {//Your contact info Git hub UserName
 
         type: "input",
-        name: "Github UserName",
+        name: "Github",
         message: "What is your GitHub user name?",
     },
-    {
+    {//The technologies being used ont the project
 
         type: "checkbox",
         name: "Technology",
         message: "What type of technology did you use on the project?",
         choices: ["HTML","Javacript","Node", "CSS", "JQuery","Node"]
     },
-    {
+    {//The usage of the project
 
         type: "input",
         name: "Description",
         message: "What is a short description of your project?",
     },
  
-    {
+    {//The license of the project should have links
 
         type: "list",
         name: "License",
         message: "What type of license should your project have?",
         choices: ["MIT", "Apache 2.0","GPL 3.0","BSD 3","None"]
     },
-    {
+    {//The dependencies used for the project
 
         type: "input",
-        name: "Dependencies",
+        name: "installation",
         message: "What command should be used to install dependencies?",
     },
-    {
+    {//The Test
 
         type: "input",
         name: "Test",
         message: "What command should be used to run tests?",
     },
-    {
+    {///Additional info on the Repo
 
         type: "input",
         name: "Repo Info",
@@ -74,8 +80,8 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, json.stringify(data, null, "/n"), (err) => err ? 
-    console.error(err) : console.log("The File Was written!!"));
+    fs.writeFile("README.md", README, (err) =>
+            err ? console.error(err) : console.log("file Created"));
 }
 
 // TODO: Create a function to initialize app
